4b529152336351e72e885304d83a3adb05069003
nutszebra <nutszebra@gmail.com>
2018-10-24 23:58:44 +0900

    sh

--- main.sh
+++ main.sh
@@ -10,7 +10,6 @@ DIR="/datasets01/imagenet_full_size/061417/train"
 ARCH="alexnet"
 LR=0.05
 WD=-5
-K=10000
 WORKERS=12
 EXP="/private/home/${USER}/test/exp"
 PYTHON="/private/home/${USER}/test/conda/bin/python"
@@ -18,4 +17,4 @@ PYTHON="/private/home/${USER}/test/conda/bin/python"
 mkdir -p ${EXP}
 
 CUDA_VISIBLE_DEVICES=0 ${PYTHON} main.py ${DIR} --exp ${EXP} --arch ${ARCH} \
-  --lr ${LR} --wd ${WD} --k ${K} --sobel --verbose --workers ${WORKERS}
+  --lr ${LR} --wd ${WD} --sobel --verbose --workers ${WORKERS}
