34b5593a51390f30f524473173d83230a99d2c87
nutszebra <nutszebra@gmail.com>
2018-10-24 18:38:32 +0900

    averagemeter

--- util.py
+++ util.py
@@ -44,6 +44,24 @@ def load_model(path):
     return model
 
 
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
 class Logger():
     """ Class to update every epoch to keep trace of the results
     Methods:
