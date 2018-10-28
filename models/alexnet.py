1991bd1ff46d794920f5c6d21dfd5deb3bc14785
nutszebra <nutszebra@gmail.com>
2018-10-29 00:12:43 +0900

    0.05

--- models/alexnet.py
+++ models/alexnet.py
@@ -151,7 +151,7 @@ def make_layers_features(cfg, input_dim, bn):
     return nn.Sequential(*layers)
 
 
-def alexnet(sobel=False, bn=True, out=32, length_train=None, alpha=1.0e-2, memory_dim=10000, momentum=0.01, reassign_period=200, beta=1.0e-3):
+def alexnet(sobel=False, bn=True, out=32, length_train=None, alpha=1.0e-2, memory_dim=10000, momentum=0.05, reassign_period=200, beta=1.0e-3):
     dim = 2 + int(not sobel)
     model = AlexNet(make_layers_features(CFG['2012'], dim, bn=bn), out, sobel, length_train, alpha, memory_dim, momentum, reassign_period, beta)
     return model
