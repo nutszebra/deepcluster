e41443a7bf74c5b46efb519f47b1a3ad60159e8e
nutszebra <nutszebra@gmail.com>
2018-10-29 13:23:58 +0900

    detach

--- models/alexnet.py
+++ models/alexnet.py
@@ -120,7 +120,7 @@ class AlexNet(nn.Module):
         # t = self.embedding.weight[index_of_min_embedding]
         # loss = cross_entropy.softmax_cross_entropy(y, F.softmax(t, 1), average=True, reduce=True)
         # loss_push = cross_entropy.softmax_cross_entropy(torch.cat((t[1:], t[:1])), F.softmax(t, 1), average=True, reduce=True)
-        loss_push2 = cross_entropy.softmax_cross_entropy(torch.cat((y[1:], y[:1])), F.softmax(y, 1), average=True, reduce=True)
+        loss_push2 = cross_entropy.softmax_cross_entropy(torch.cat((y[1:], y[:1])), F.softmax(y, 1).detach(), average=True, reduce=True)
         # return loss - self.alpha * loss_push - self.beta * loss_push2
         # return loss + self.alpha * loss_push - self.beta * loss_push2
         return - loss_push2
