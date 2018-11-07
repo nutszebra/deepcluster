d00a1e54fb7fb5b26f694f1966bf1ee5a47cc1eb
nutszebra <nutszebra@gmail.com>
2018-11-07 15:17:15 +0900

    numerical stability

--- models/cross_entropy.py
+++ models/cross_entropy.py
@@ -2,13 +2,14 @@
 
 class SoftmaxCrossEntropy(object):
 
-    def __init__(self, average=True, reduce=True):
+    def __init__(self, average=True, reduce=True, eps=1.0e-10):
+        self.eps = eps
         self.average = average
         self.reduce = reduce
 
     def __call__(self, y, t):
         batch, num_class = y.data.shape
-        loss = t * (- y + y.exp().sum(1).log().repeat(1, num_class).view(num_class, batch).transpose(0, 1))
+        loss = - t * torch.log(F.softmax(y, 1) + self.eps)
         divider = batch if self.average is True else 1.0
         if self.reduce is True:
             return loss.sum() / divider
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     import torch.nn.functional as F
     w = torch.nn.Parameter(torch.FloatTensor([[0.1, 0.2, 0.3, 0.4, 0.5], [0.1, 0.1, 0.1, 0.1, 0.1]]))
     t = torch.FloatTensor([[0.2, 0.1, 0.1, 0.5, 0.1], [0.2, 0.1, 0.1, 0.5, 0.1]])
-    optimizer = torch.optim.SGD([w], lr=0.1)
+    optimizer = torch.optim.SGD([w], lr=0.01)
     for i in range(20000):
         loss = softmax_cross_entropy(w, t)
         optimizer.zero_grad()
